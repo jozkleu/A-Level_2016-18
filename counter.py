4467312aa6a07b97954ac535a2e87bbfab0ac21c
jozkleu <1090@shenfield.essex.sch.uk>
2016-10-20 18:49:19 +0100

    Add files via upload
    not sure if this was what we were supposed to do or not

---
+++ counter.py
@@ -0,0 +1,14 @@
+one = 1
+ten = 10
+counter = 0
+
+counter = one
+print(counter)
+while counter > 0:
+    if counter < 10:
+        counter = counter + one
+        print(counter)
+    else:
+        break
+
+        
