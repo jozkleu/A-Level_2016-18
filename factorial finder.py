5465e9da00d35b2b8db1c2eda862cb0e8f73fbfd
jozkleu <1090@shenfield.essex.sch.uk>
2016-12-02 10:09:36 +0000

    Add files via upload

---
+++ factorial finder.py
@@ -0,0 +1,20 @@
+#Jozua Kleu 01/02/16
+#factorial finder
+
+base_number = int(input("enter the base number to calculate the factorials of"))
+counter = base_number 
+##while counter != 1:
+##    counter = counter - 1
+##    base_number = base_number * counter
+##print (base_number)
+
+def factorial(n):
+    if base_number == 0:
+        return 1
+    else:
+        return
+    base_number * factorial(base_number - 1)
+    print (base_number)
+
+    
+    
